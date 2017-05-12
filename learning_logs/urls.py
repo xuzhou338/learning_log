53d0a765791acba17eff094ea9cb76c19c827c48
JoJoX <xuzhou338@gmail.com>
2017-05-12 14:49:10 -0400

    Finished Ch.18

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -8,5 +8,8 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
 
     # Show all topics.
-    url(r'^topics/$', views.topics, name='topics')
+    url(r'^topics/$', views.topics, name='topics'),
+
+    # Detail page for a single topic
+    url(r'^topics/(?P<topic_id>\d+)/$', views.topic, name='topic'),
 ]
